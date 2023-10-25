18c4179f5e80cf7ef25a1c074fb2ccfa5db30010
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-10-25 17:14:34 +0700

    src:app: turn off deprecation warnings

--- src/app/viewport.hpp
+++ src/app/viewport.hpp
@@ -1,6 +1,8 @@
 #ifndef VIEWPORT_HPP
 #define VIEWPORT_HPP
 
+#define GL_SILENCE_DEPRECATION
+
 #include <QColor>
 #include <QMouseEvent>
 #include <QOpenGLFunctions>
@@ -12,23 +14,24 @@ class Viewport : public QOpenGLWidget, protected QOpenGLFunctions {
   Q_OBJECT
 
  private:
-  //  ObjViewer_Mesh m_mesh;
-  //  ObjViewer_Matrix4x4 m_model;
-  //  ObjViewer_Matrix4x4 m_view;
-  //  ObjViewer_Matrix4x4 m_projection;
+  ObjViewerMesh mesh;
+  //  ObjViewerMatrix4x4 m_model;
+  //  ObjViewerMatrix4x4 m_view;
+  //  ObjViewerMatrix4x4 m_projection;
 
-  // display settings
+  // Display settings
   QColor background_color = QColor("#EFE5D7");
   QColor line_color = QColor("#974F4C");
   QColor vertex_color = line_color;
   float line_width;
   float vertex_size;
-  // mouse camera control
+  // Mouse camera control
+  float camera_speed = 0.2;
   QPoint mouse_pos;
-  float mouse_speed = 0.2;
   float mouse_rotx = -30;
   float mouse_roty = -30;
   float mouse_rotz = 0;
+  // Window settings
   int w, h;
 
  public:
@@ -40,22 +43,26 @@ class Viewport : public QOpenGLWidget, protected QOpenGLFunctions {
   ~Viewport();
 
  private:
+  // Template functions
   void initializeGL() override;
   void resizeGL(int w, int h) override;
   void paintGL() override;
-
+  // Helpers
+  void paintObject(const ObjViewerMesh &);
+  void drawCube(float, float, float, float);
+  // Events
   void mousePressEvent(QMouseEvent *) override;
   void mouseMoveEvent(QMouseEvent *) override;
 
  public:
-  // color
+  // Color
   void setBackgroundColor(const QColor &color) { background_color = color; }
   void setLineColor(const QColor &color) { line_color = color; }
   void setVertexColor(const QColor &color) { vertex_color = color; }
   QColor getBackgroundColor() const { return background_color; }
   QColor getLineColor() const { return line_color; }
   QColor getPointColor() const { return vertex_color; }
-  // width/size
+  // Primitives' width/size
   void setVertexSize(float size) { vertex_size = size; }
   void setLineWidth(float width) { line_width = width; }
   float getVertexSize() const { return vertex_size; }
