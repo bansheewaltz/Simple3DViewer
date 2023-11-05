6f49c37f20c77cb08d754921d6ba0dc63f0cb458
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-11-05 21:55:34 +0700

    project structure: remove unnecessary folder structures, rename files

--- src/lib/src/parsing/external.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXTERNAL_H_
-#define EXTERNAL_H_
-
-#include "../../extern/fast_obj/fast_obj.h"
-
-#endif  // EXTERNAL_H_
--- src/lib/test/faces_to_lines.cpp
+++
@@ -1,6 +0,0 @@
-#include "obj_viewer.h"
-
-int main() {
-  ObjViewerMesh* m = objviewer_create_cube(0, 0, 0, 1);
-  unsigned int* arr = objviewer_faces_to_lines(m);
-}
